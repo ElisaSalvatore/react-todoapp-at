dea701e87dae57ce9b780f8bcdba37470b67d96d
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-07-01 10:46:40 +0200

    mods todo using functional based components (all in App)

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
-import React from 'react';
-import Todo from './components/Todo.js';
-import Form from './components/Form.js';
-
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    todos: [
-      {name:"imparare React", completato: false},
-      {name:"imparare gli state", completato: false},
-      {name:"imparare i component", completato: false},
-    ]
+//COMPONENTE TODO
+const Todo = (props) => {
+  const {todo} = props;
+  return (
+    <div style={{textDecoration: todo.completato ? 'line-through' : ''}} className='todo'>
+      {todo.name}
+      <div>
+        <button onClick={() => props.completaTodo(props.index)}> done </button>
+      </div>  
+    </div>
+  )
+}
+
+//COMPONENTE FORM
+const Form = (props) => {
+  const [value, setValue] = useState();
+  //passare attraverso il submit il valore dell'input, e passarlo quindi ad un prop in App/Form
+  const handleSubmit = (e) => {
+    e.preventDefault(); //così il todo appena inserito non si cancella subito
+
+    if(value.trim() === ''){
+      alert("scrivi un todo")
+      return;
+    }
+    props.submit(value);
+    setValue(''); //input torna vuoto una volta aggiunto il todo
   }
 
-  addTodo = (todo) => {
-    const newTodos = [...this.state.todos, {name: todo}];
-    this.setState({
-      todos: newTodos
-    })
+  //cambiare lo state del nuovo todo con ciòc he inserisce l'utente (prima era stringa vuota)
+  const onChangeText = (e) => {
+      //console.log(e.target.value)
+      setValue(e.target.value);
   }
-
-  completaTodo = (index) => {
-    const newTodos = [...this.state.todos];
-    newTodos[index].completato = true;
-    this.setState({
-      todos: newTodos
-    })
-  }
-
-  render() {
-    return (
-       <div className='app'>
-          <div className='todo-list'>
-            {this.state.todos.map((todo, index) => (
-              <Todo key={index} todo={todo} index={index} completaTodo={this.completaTodo}/>
-            ))}
-            <Form submit={this.addTodo}/>
-          </div>
-       </div> 
-    )
   
+  return (
+  <form onSubmit={handleSubmit}>
+      <input className="input" type="text" value={value} placeholder="aggiungi todo" onChange={onChangeText}/>
+  </form>
+  )
+}
+
+//COMPONENTE APP
+const App = () => {
+  const [todos, setTodos] = useState([
+    { name: "imparare React", completato: false },
+    { name: "imparare gli state", completato: false },
+    { name: "imparare i component", completato: false },
+  ])
+
+  const addTodo = (todo) => {
+    const newTodos = [...todos, { name: todo }];
+    setTodos(newTodos);
   }
+
+  const completaTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completato = true;
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((item, index) => (
+          <Todo key={index} todo={item} index={index} completaTodo={completaTodo} />
+        ))}
+        <Form submit={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
